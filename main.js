dc97e292ad3be869687898b1c26e8fe991318427
BrendenLancrae <44102413+BrendenLancrae@users.noreply.github.com>
2024-01-17 01:52:14 -0500

    Streamlined the game function.

--- main.js
+++ main.js
@@ -1,38 +1,62 @@
-   let ro = "rock";
-   let pa = "paper";
-   let sc = "scissors";
+let ro = "rock";
+let pa = "paper";
+let sc = "scissors";
 
-function game(userInput) {
+
+function game(playerSelection) {
 
     const rpsArray = ["rock", "paper", "scissors"];
-    const computerChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+    const getComputerChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
 
-    console.log("User Input", userInput);
-    console.log("Computer Choice", computerChoice);
+    console.log("The computer chose " + getComputerChoice)
 
-    if (userInput === ro && computerChoice === pa) {
-        console.log( "You lose!" );
-    } else if (userInput === ro && computerChoice === ro) {
-        console.log( "You tied!" );
-    } else if (userInput === ro && computerChoice === sc) {
-        console.log ( "You win!" );
-    } else if (userInput === pa && computerChoice === sc) {
-        console.log( "You lose!" );
-    } else if (userInput === pa && computerChoice === pa) {
-        console.log ( "You tied!" );
-    } else if (userInput === pa && computerChoice === ro) {
-        console.log( "You win!" );
-    } else if (userInput === sc && computerChoice === ro) {
-        console.log ( "You lose!" );
-    } else if (userInput === sc && computerChoice === pa) {
-        console.log ( "You win!" );
-    } else if (userInput === sc && computerChoice === sc) {
-        console.log ( "You tied!");
+    if (playerSelection === ro && getComputerChoice === sc || playerSelection === pa && getComputerChoice === ro || playerSelection === sc && getComputerChoice === pa) {
+        return "You win.";
+    } else if (playerSelection === ro && getComputerChoice === ro || playerSelection === sc && getComputerChoice === sc || playerSelection === pa && getComputerChoice === pa) {
+        return  "You tied.";
+    } else {
+        return "You lose!";
     }
 }
 
-const userInput = prompt( "Please choose rock, paper, or scissors." ).toLowerCase();
-game(userInput);
-/* Take the userInput and hold it until getComputerChoice
-then compare those two
-rock < paper < scissors < rock */
+const playerSelection = prompt( "Please choose rock, paper, or scissors." ).toLowerCase();
+
+console.log("You chose " + playerSelection)
+
+
+game(playerSelection);
+
+console.log(game(playerSelection));
+
+
+// the original else if statements that ran the game
+
+/* function game(playerSelection) {
+
+    const rpsArray = ["rock", "paper", "scissors"];
+    const getComputerChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+
+    console.log("User Input", playerSelection);
+    console.log("Computer Choice", getComputerChoice);
+
+    if (playerSelection === ro && getComputerChoice === pa) {
+        console.log( "You lose!" );
+    } else if (playerSelection === ro && getComputerChoice === ro) {
+        console.log( "You tied!" );
+    } else if (playerSelection === ro && getComputerChoice === sc) {
+        console.log ( "You win!" );
+    } else if (playerSelection === pa && getComputerChoice === sc) {
+        console.log( "You lose!" );
+    } else if (playerSelection === pa && getComputerChoice === pa) {
+        console.log ( "You tied!" );
+    } else if (playerSelection === pa && getComputerChoice === ro) {
+        console.log( "You win!" );
+    } else if (playerSelection === sc && getComputerChoice === ro) {
+        console.log ( "You lose!" );
+    } else if (playerSelection === sc && getComputerChoice === pa) {
+        console.log ( "You win!" );
+    } else if (playerSelection === sc && getComputerChoice === sc) {
+        console.log ( "You tied!");
+    }
+} */
+
